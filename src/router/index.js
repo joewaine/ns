4c406f0f382617f8d655db19e15310953420b630
joe waine <joewaine@wawa1114s-imac.local>
2020-06-11 14:02:32 -0700

    add up

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/home",
+    name: "home",
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("../views/register.vue")
+  },
+  {
+    path: "/addproduct",
+    name: "addproduct",
+    component: () => import("../views/addproduct.vue")
+  },
+
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem("jwt") == null) {
+      next({
+        path: "/"
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
---
+++ src/views/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    <Nav />
+    <login />
+    <Products />
+  </div>
+</template>
+<script>
+import login from "@/components/auth/login";
+import Products from "@/components/Products";
+import Nav from "@/components/Nav";
+export default {
+  components: {
+    login,
+    Products,
+    Nav
+  }
+};
+</script>
